c6407bb74a65ff57b71b7e38f1358a8e8d4e4feb
amiritahmed <amiritahmed@gmail.com>
2025-03-21 15:14:38 -0400

    changes to driver class

--- src/test/java/com/demo/pages/AmazonHomePage.java
+++ src/test/java/com/demo/pages/AmazonHomePage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class AmazonHomePage {
-
+//this is dedicated for amazon home page
     @FindBy(id = "twotabsearchtextbox")
     private WebElement searchBox;
 
